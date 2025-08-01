c2d9158a7fe57f930deb9e7db3d1976a0281abca
Nicolás Ceglán <nicozilabe@hotmail.com>
2025-07-31 21:10:32 -0300

    Refactor Sistema class to implement singleton pattern

--- js/system/sistema.js
+++ js/system/sistema.js
@@ -1,16 +1,26 @@
-//sistema, osea el "backend".
-
 class Sistema {
   constructor() {
+    if (Sistema.instance) {
+      return Sistema.instance;
+    }
+
     this.administradores = [];
     this.barberos = [];
     this.reservas = [];
     this.servicios = [];
+
+    Sistema.instance = this;
   }
 
   saludar() {
     alert("Sistema iniciado");
   }
-
-
 }
+
+
+const sistema1 = new Sistema();
+const sistema2 = new Sistema();
+
+console.log(sistema1 === sistema2); 
+
+
